10e91c05b7dbf3948364d917dd2799242cc75ba8
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2023-05-10 19:13:56 -0500

    part4

---
+++ CosmicSeaRebellion/Assets/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    WeaponController weaponController;
+
+    public WeaponObject rightWeapon;
+    public WeaponObject leftWeapon;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        weaponController = GetComponentInChildren<WeaponController>();
+    }
+
+    void Start()
+    {
+        weaponController.LoadWeapondSlot(rightWeapon, false);
+        weaponController.LoadWeapondSlot(leftWeapon, true);
+    }
+}
---
+++ CosmicSeaRebellion/Assets/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public HealthPlayer health;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<HealthPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
