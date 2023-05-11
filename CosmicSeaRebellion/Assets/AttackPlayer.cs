58e90fe3b781603f2263118047fc408260f9c0f4
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2023-05-10 19:03:26 -0500

    First update part1

---
+++ CosmicSeaRebellion/Assets/AttackPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+    public int lastAttack;
+
+    public void HandleLightAttack(WeaponObject weapon)
+    {
+        Player.Instance.animationPlayer.anim.SetInteger("WeaponIndex", weapon.ligthAttackIndex);
+        Player.Instance.animationPlayer.PlayTargetAnimationRootMotion("Attack", true);
+        lastAttack = weapon.ligthAttackIndex;
+    }
+
+    public void HandleWeaponCombo(WeaponObject weapon)
+    {
+
+        if (Player.Instance.walkerController.comboFlag)
+        {
+            Player.Instance.animationPlayer.SetAnimationBool("CanDoCombo", false);
+
+            if (lastAttack == weapon.ligthAttackIndex)
+            {
+                Player.Instance.animationPlayer.anim.SetInteger("WeaponIndex", weapon.heavyAttackIndex);
+                Player.Instance.animationPlayer.PlayTargetAnimationRootMotion("Attack", true);
+            }
+        }
+    }
+
+    public void HandleHeavytAttack(WeaponObject weapon)
+    {
+        Player.Instance.animationPlayer.anim.SetInteger("WeaponIndex", weapon.heavyAttackIndex);
+        Player.Instance.animationPlayer.PlayTargetAnimationRootMotion("Attack", true);
+        lastAttack = weapon.heavyAttackIndex;
+    }
+} 
---
+++ CosmicSeaRebellion/Assets/DamageCollider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    [SerializeField] Collider damageCollider;
+    public int currentWeaponDamage;
+
+    void Awake()
+    {
+        damageCollider = GetComponentInChildren<Collider>();
+        damageCollider.gameObject.SetActive(true);
+        damageCollider.isTrigger = true;
+        damageCollider.enabled = false;
+    }
+
+    public void ChangeDamageCollider()
+    {
+        damageCollider.enabled = !damageCollider.enabled;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.PLAYER_TAG))
+        {
+            if (Player.Instance.PlayerStats != null)
+            {
+                Player.Instance.PlayerStats.health.TakeDamage(currentWeaponDamage);
+            }
+        }
+
+        if (other.CompareTag(Tags.ENEMY_TAG))
+        {
+            EnemyHealth enemyStats = other.GetComponent<EnemyHealth>();
+
+            if (enemyStats != null)
+            {
+                enemyStats.TakeDamage(currentWeaponDamage,damageCollider.transform.position);
+            }
+        }
+    }
+}
